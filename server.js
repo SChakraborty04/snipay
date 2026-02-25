7a6c81ef05eb7defae5b2d5e40d0fc1d9366a4ca
SChakraborty04 <shaanch04@yahoo.com>
2026-02-25 13:16:41 +0530

    updated PORT for caprover auto PORT assignment

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectToDB = require('./src/config/db');
 
+const PORT = process.env.PORT || 3001;
+
 connectToDB();
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
